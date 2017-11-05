81ce262611fb1b3945b60aea861f44cafeb82dcd
Luis Gonzalez <luisarmando1234@gmail.com>
2017-11-04 22:48:49 -0600

    Added main code

--- Paypal.php
+++ Paypal.php
@@ -3,26 +3,22 @@
  * File Paypal.php.
  *
  * @author Marcio Camello <marciocamello@outlook.com>
+ * @author Luis Gonzalez INQUID <contact@inquid.co>
  * @see https://github.com/paypal/rest-api-sdk-php/blob/master/sample/
  * @see https://developer.paypal.com/webapps/developer/applications/accounts
  */
+namespace common\components;
 
-namespace marciocamello\yii2-paypal;
-
-define('PP_CONFIG_PATH', __DIR__);
-
+use Exception;
+use PayPal\Api\PaymentExecution;
 use Yii;
 use yii\base\ErrorException;
 use yii\helpers\ArrayHelper;
 use yii\base\Component;
-
-use PayPal\Api\Address;
-use PayPal\Api\CreditCard;
 use PayPal\Api\Amount;
 use PayPal\Api\Payer;
 use PayPal\Api\Payment;
 use PayPal\Api\Transaction;
-use PayPal\Api\FundingInstrument;
 use PayPal\Auth\OAuthTokenCredential;
 use PayPal\Api\Details;
 use PayPal\Api\Item;
@@ -34,32 +30,30 @@ class Paypal extends Component
 {
     //region Mode (production/development)
     const MODE_SANDBOX = 'sandbox';
-    const MODE_LIVE    = 'live';
+    const MODE_LIVE = 'live';
     //endregion
-
     //region Log levels
     /*
      * Logging level can be one of FINE, INFO, WARN or ERROR.
      * Logging is most verbose in the 'FINE' level and decreases as you proceed towards ERROR.
      */
-    const LOG_LEVEL_FINE  = 'FINE';
-    const LOG_LEVEL_INFO  = 'INFO';
-    const LOG_LEVEL_WARN  = 'WARN';
+    const LOG_LEVEL_FINE = 'FINE';
+    const LOG_LEVEL_INFO = 'INFO';
+    const LOG_LEVEL_WARN = 'WARN';
     const LOG_LEVEL_ERROR = 'ERROR';
     //endregion
-
     //region API settings
     public $clientId;
     public $clientSecret;
-    public $isProduction = false;
-    public $currency = 'USD';
+    public $isProduction;
+    public $currency;
     public $config = [];
 
     /** @var ApiContext */
     private $_apiContext = null;
 
     /**
-     * @setConfig 
+     * @setConfig
      * _apiContext in init() method
      */
     public function init()
@@ -67,9 +61,6 @@ class Paypal extends Component
         $this->setConfig();
     }
 
-    /**
-     * @inheritdoc
-     */
     private function setConfig()
     {
         // ### Api context
@@ -92,15 +83,14 @@ class Paypal extends Component
         // based configuration
         $this->_apiContext->setConfig(ArrayHelper::merge(
             [
-                'mode'                      => self::MODE_SANDBOX, // development (sandbox) or production (live) mode
-                'http.ConnectionTimeOut'    => 30,
-                'http.Retry'                => 1,
-                'log.LogEnabled'            => YII_DEBUG ? 1 : 0,
-                'log.FileName'              => Yii::getAlias('@runtime/logs/paypal.log'),
-                'log.LogLevel'              => self::LOG_LEVEL_FINE,
-                'validation.level'          => 'log',
-                'cache.enabled'             => 'true'
-            ],$this->config)
+                'http.ConnectionTimeOut' => 30,
+                'http.Retry' => 1,
+                'log.LogEnabled' => YII_DEBUG ? 1 : 0,
+                'log.LogLevel' => self::LOG_LEVEL_FINE,
+                'validation.level' => 'log',
+                'cache.enabled' => 'true',
+                'log.FileName' => Yii::getAlias('@runtime/logs/paypal.log'),
+            ], $this->config)
         );
 
         // Set file name of the log if present
@@ -124,52 +114,141 @@ class Paypal extends Component
         return $this->_apiContext;
     }
 
-    //Demo
-    public function payDemo()
+    public function payer($tipo)
     {
-        $addr = new Address();
-        $addr->setLine1('52 N Main ST');
-        $addr->setCity('Johnstown');
-        $addr->setCountryCode('US');
-        $addr->setPostalCode('43210');
-        $addr->setState('OH');
-
-        $card = new CreditCard();
-        $card->setNumber('4417119669820331');
-        $card->setType('visa');
-        $card->setExpireMonth('11');
-        $card->setExpireYear('2018');
-        $card->setCvv2('874');
-        $card->setFirstName('Joe');
-        $card->setLastName('Shopper');
-        $card->setBillingAddress($addr);
-
-        $fi = new FundingInstrument();
-        $fi->setCreditCard($card);
-
         $payer = new Payer();
-        $payer->setPaymentMethod('credit_card');
-        $payer->setFundingInstruments(array($fi));
-
-        $amountDetails = new Details();
-        $amountDetails->setSubtotal('15.99');
-        $amountDetails->setTax('0.03');
-        $amountDetails->setShipping('0.03');
-
-        $amount = new Amount();
-        $amount->setCurrency('USD');
-        $amount->setTotal('7.47');
-        $amount->setDetails($amountDetails);
-
-        $transaction = new Transaction();
-        $transaction->setAmount($amount);
-        $transaction->setDescription('This is the payment transaction description.');
-
-        $payment = new Payment();
-        $payment->setIntent('sale');
-        $payment->setPayer($payer);
-        $payment->setTransactions(array($transaction));
-
-        return $payment->create($this->_apiContext);
+        $payer->setPaymentMethod($tipo);
+        return $payer;
     }
+
+    public function item($arrayParam)
+    {
+        $item = new Item();
+        $item->setName($arrayParam['concepto'])
+            ->setCurrency($this->currency)
+            ->setQuantity($arrayParam['cantidad'])
+            ->setPrice($arrayParam['total']);
+        return $item;
+    }
+
+    public function itemList($arrayItem)
+    {
+        $itemList = new ItemList();
+        $itemList->setItems($arrayItem);
+        return $itemList;
+    }
+
+    public function details($arrayDetails)
+    {
+        $details = new Details();
+        $details->setShipping($arrayDetails['shipping'])
+            ->setTax($arrayDetails['tax'])
+            ->setSubtotal($arrayDetails['subtotal']);
+        return $details;
+    }
+
+    public function amount($arrayAmount)
+    {
+        $amount = new Amount();
+        $amount->setCurrency($this->currency)
+            ->setTotal($arrayAmount['total'])
+            ->setDetails($arrayAmount['details']);
+        return $amount;
+    }
+
+    public function transaction($arrayTransaction)
+    {
+        $transaction = new Transaction();
+        $transaction->setAmount($arrayTransaction['amount'])
+            ->setItemList($arrayTransaction['itemList'])
+            ->setDescription($arrayTransaction['descripcion'])
+            ->setInvoiceNumber($arrayTransaction['uniqid']);
+        return $transaction;
+    }
+
+    public function redirectUrls($ruta)
+    {
+        $redirectUrls = new RedirectUrls();
+        $redirectUrls->setReturnUrl($ruta . '&successPaypal=1')
+            ->setCancelUrl($ruta . '&errorPaypal=1');
+        return $redirectUrls;
+    }
+
+    public function payment($arrayPyment)
+    {
+        $payment = new Payment();
+        $payment->setIntent($arrayPyment['intent'])
+            ->setPayer($arrayPyment['payer'])
+            ->setRedirectUrls($arrayPyment['redirectUrls'])
+            ->setTransactions($arrayPyment['transaction']);
+        $request = clone $payment;
+
+        try {
+            $payment->create($this->_apiContext);
+        } catch (Exception $ex) {
+            return $ex;
+        }
+
+        return $approvalUrl = $payment->getApprovalLink();
+    }
+
+	public function address($arrayAddress){
+		$addr = new Address();
+		$addr->setLine1($arrayPyment['line1']);
+		$addr->setCity($arrayPyment['city']);
+		$addr->setCountryCode($arrayPyment['country']);
+		$addr->setPostalCode($arrayPyment['postal_code']);
+		$addr->setState($arrayPyment['state']);	
+		return $addr;
+	}
+
+	public function card($arrayCard){
+		$card = new CreditCard();
+		$card->setNumber(arrayCard['number']);
+		$card->setType(arrayCard['type']);
+		$card->setExpireMonth(arrayCard['exp_month']);
+		$card->setExpireYear(arrayCard['exp_year']);
+		$card->setCvv2(arrayCard['cvv2']);
+		$card->setFirstName(arrayCard['first_name']);
+		$card->setLastName(arrayCard['last_name']);
+		$card->setBillingAddress(arrayCard['addr']);
+		return $card;
+	}
+
+
+
+
+
+    
+
+    public function execute($paymentId, $payerId, $shipping, $tax, $subtotal, $total)
+    {
+        $payment = Payment::get($paymentId, $this->_apiContext);
+        $execution = new PaymentExecution();
+        $execution->setPayerId($payerId);
+        $transaction = new Transaction();
+        $amount = new Amount();
+        $details = new Details();
+        $details->setShipping($shipping)
+            ->setTax($tax)
+            ->setSubtotal($subtotal);
+        $amount->setCurrency($this->currency);
+        $amount->setTotal($total);
+        $amount->setDetails($details);
+        $transaction->setAmount($amount);
+        $execution->addTransaction($transaction);
+        try {
+            $result = $payment->execute($execution, $this->_apiContext);
+            try {
+                $payment = Payment::get($paymentId, $this->_apiContext);
+                return true;
+            } catch (Exception $ex) {
+                print_r($ex);
+                return false;
+            }
+        } catch (Exception $ex) {
+            return false;
+        }
+    }
+
 }
