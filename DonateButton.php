27a0c89601bef7499e42bfb187887b0250e57214
Luis Gonzalez <luisarmando1234@gmail.com>
2018-02-21 00:01:59 -0600

    Create DonateButton.php
    TODO add all other options

---
+++ DonateButton.php
@@ -0,0 +1,22 @@
+<?php
+namespace app\components;
+
+class PaypalButtonWidget extends Widget
+{
+    public $code;
+    
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function run()
+    {
+        return '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="'.$code.'">
+<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+</form>';
+    }
+}
